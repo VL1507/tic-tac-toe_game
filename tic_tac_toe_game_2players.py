e478cf8ce62338389f7f398fc0758d2e2d362810
VL1507 <88646034+VL1507@users.noreply.github.com>
2022-10-09 22:15:38 +1200

    Update tic_tac_toe_game_2players.py

--- tic_tac_toe_game_2players.py
+++ tic_tac_toe_game_2players.py
@@ -1,47 +1,46 @@
-l = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
-c = 0
-for i in l:
-    print(*i, sep=' | ')
-    if c != 2:
-        print('- - - - -')
-        c += 1
+# крестики-нолики
 
 
-lst = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-k = 0  # номер игрока
-while True:
-    n = input("Игрок номер {}, сделайте ход\t".format(k % 2 + 1))
-         
-    if n == "стоп":
-        break
-    
-    n = int(n)
-    
-    if not (1 <= n <= 9):
-        print("Введите число от 1 до 9")
-        continue
-    if lst[(n - 1) // 3][n % 3 - 1] != " ":
-        print('Это поле уже занято')
-        continue
-
-    
-
-    lst[(n - 1) // 3][n % 3 - 1] = ('X', 'O')[k % 2]
-
+def print_board(board: list) -> str:
     c = 0
-    for i in lst:
+    for i in board:
         print(*i, sep=' | ')
         if c != 2:
             print('- - - - -')
             c += 1
 
-    if (lst[0][0] == lst[0][1] == lst[0][2] !=' ') or (lst[1][0] == lst[1][1] == lst[1][2] != ' ') or (lst[2][0] == lst[2][1] == lst[2][2] !=' ') \
-        or (lst[0][0] == lst[1][0] == lst[2][0] != ' ') or (lst[0][1] == lst[1][1] == lst[2][1] !=' ') or (lst[0][2] == lst[1][2] == lst[2][2] != ' ') \
-            or (lst[0][0] == lst[1][1] == lst[2][2] !=' ') or (lst[0][2] == lst[1][1] == lst[2][0] != ' '):
+
+print_board([["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]])
+
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+k = 0  # номер игрока
+
+while True:
+    n = input("Игрок номер {}, сделайте ход\t".format(k % 2 + 1))
+
+    if n == "стоп":
+        break
+
+    n = int(n)
+
+    if not (1 <= n <= 9):
+        print("Введите число от 1 до 9")
+        continue
+    if board[(n - 1) // 3][n % 3 - 1] != " ":
+        print('Это поле уже занято')
+        continue
+
+    board[(n - 1) // 3][n % 3 - 1] = ('X', 'O')[k % 2]
+
+    print_board(board)
+
+    if (board[0][0] == board[0][1] == board[0][2] != ' ') or (board[1][0] == board[1][1] == board[1][2] != ' ') or (board[2][0] == board[2][1] == board[2][2] != ' ') \
+        or (board[0][0] == board[1][0] == board[2][0] != ' ') or (board[0][1] == board[1][1] == board[2][1] != ' ') or (board[0][2] == board[1][2] == board[2][2] != ' ') \
+            or (board[0][0] == board[1][1] == board[2][2] != ' ') or (board[0][2] == board[1][1] == board[2][0] != ' '):
         print('Выиграл игрок {}!'.format(k % 2 + 1))
         break
-    
-    if sum([i.count(" ") for i in lst]) == 0:
+
+    if sum([i.count(" ") for i in board]) == 0:
         print("Ничья")
         break
     k += 1
