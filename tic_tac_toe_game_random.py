1755da9a393a2a796555be409c3fc2d596085c07
VL1507 <88646034+VL1507@users.noreply.github.com>
2022-10-10 14:57:49 +1200

    Update tic_tac_toe_game_random.py

--- tic_tac_toe_game_random.py
+++ tic_tac_toe_game_random.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep
 
 
-def print_board(board: list) -> None:
+def print_board(board: list):
     c = 0
     for i in board:
         print(*i, sep=' | ')
